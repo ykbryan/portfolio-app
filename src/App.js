a457d2e8928e335560cb8e93e336f09c92979feb
Bryan Chua <me@bryanchua.com>
2018-08-27 21:29:50 +0800

    Testing CI/CD

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">bryanchua.com</h1>
         </header>
         <p className="App-intro">
-          Hello World.
+          Hello World. Hello World.
         </p>
       </div>
     );
